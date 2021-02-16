33315eb7570b404e7be6d111684ed50d6310b11e
tyanmahou <tyanmahou@gmail.com>
2021-02-17 03:04:47 +0900

    update example

--- MahoRx/examples/Where/main.cpp
+++ MahoRx/examples/Where/main.cpp
@@ -11,30 +11,17 @@ int main()
     auto subject = std::make_shared<Subject<int>>();
 
     subject | where([](int a) {
-        if (a == 3) {
-            throw std::exception("The three");
-        }
         return a > 0;
     }) | subscribe([](int a) {
         std::cout << a << std::endl;
-    }, [](std::exception ex) {
-        std::cout << ex.what() << std::endl;
-    }, []() {
-        std::cout << "OnComplete" << std::endl;
     });
 
     subject->onNext(1);
-    subject->onNext(2);
+    subject->onNext(-2);
     subject->onNext(3);
-    subject->onNext(4);
-
-    subject | subscribe([](int a) {
-        std::cout << a << std::endl;
-    }, []() {
-        std::cout << "Complete" << std::endl;
-    });
-    subject->onNext(5);
+    subject->onNext(-4);
     subject->onCompleted();
+    subject->dispose();
 
-    // 1, 2
+    // 1, 3
 }
